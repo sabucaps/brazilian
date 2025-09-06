efeb1e26442e5773f29f9087f93722264b8bde82
Sabu Caps <info@acer-capoeira.com>
2025-09-06 14:59:59 +0100

    Update server.js

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const express = require('express');
 const mongoose = require('mongoose');
 const cors = require('cors');
 const path = require('path');
-const bcrypt = require('bcrypt');
+const bcrypt = require('bcryptjs');
 const router = express.Router();
 
 // Models
